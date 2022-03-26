fbb8bf4070a23b875481806f2eedd2636c868357
Emir OĞUZ <ranorkk@gmail.com>
2022-03-27 00:50:15 +0300

    Update definitions.js

--- Back-End Node/library/table/definitions.js
+++ Back-End Node/library/table/definitions.js
@@ -149,7 +149,8 @@ function declareGet(res, query, rows, results, extra) {
 
 stgs.tableModules = {
   "user": new Table("tbl_user", "username", {"GET": 0, "POST": 100, "PUT": 100, "DELETE": 100}),
-  "wallet": new Table("tbl_wallet", "id", {"GET": 0, "POST": 100, "PUT": 100, "DELETE": 100})
+  "wallet": new Table("tbl_wallet", "id", {"GET": 0, "POST": 100, "PUT": 100, "DELETE": 100}),
+  "order": new Table("tbl_order", "id", {"GET": 0, "POST": 100, "PUT": 100, "DELETE": 100}),
 }
 
 module.exports = {
